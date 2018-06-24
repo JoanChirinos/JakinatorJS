6034c78d7cfb185d6f599be94f133e66ccbf7ca7
JoanChirinos <jchirinos@stuy.edu>
2018-06-23 23:01:34 -0400

    .

--- submit.py
+++ submit.py
@@ -7,7 +7,54 @@ print 'Content-type: text/html\n\n'
 
 def go():
     fs = cgi.FieldStorage()
-    for s in fs.keys():
-        print s + ': ' + fs.getvalue(s) + "<br>";
+    toAdd = []
+    
+    newQuestionMap = fs.getvalue('placeAt')[:-1]
+    if (fs.getvalue('yesOrNo') == 'yes'):
+        newResponseMap = newQuestionMap + "1"
+        oldResponseMap = newQuestionMap + "0"
+    else:
+        newResponseMap = newQuestionMap + "0"
+        oldResponseMap = newQuestionMap + "1"
+    
+    
+    straw = open("qa.txt", "rU")
+    oldFile = straw.read()
+    straw.close()
+    
+    oldFile = [i.split(",") for i in oldFile.split("\n")]
+    
+    d = {}
+    for i in oldFile:
+        d[i[0]] = i[1]
         
+    d[oldResponseMap] = d[newQuestionMap]
+    d[newQuestionMap] = "Q" + fs.getvalue('newQuestion')
+    d[newResponseMap] = "A" + fs.getvalue('newCharacter')
+    
+    toWrite = []
+    for key in d.keys():
+        toWrite += [key + "," + d[key]]
+    toWrite = '\n'.join(toWrite)
+    
+    straw = open('testQA.txt', 'w+')
+    straw.write(toWrite)
+    straw.close()
+    
+    print """
+                <html>
+                    
+                    <head>
+                        <title>College Trip Playlist</title>
+                        <script type="text/javascript">
+                            window.location.replace("index.html");
+                        </script>
+                        <link href="style.css" type="text/css" rel="stylesheet">
+                    </head>
+                    
+                    <body>
+                    </body>
+                    
+                </html>"""
+    
 go()
